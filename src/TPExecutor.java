144cd31226adb9f1b3d3d7f3057d5a371db72732
Isha803 <ishadubey2360@gmail.com>
2025-07-02 17:12:54 +0530

    shutdown, lambda expression

--- src/TPExecutor.java
+++ src/TPExecutor.java
@@ -1,29 +1,41 @@
 import java.util.concurrent.*;
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class TPExecutor {
     public static void main(String[] args) {
-        ThreadPoolExecutor executor = new ThreadPoolExecutor(
-                2,
-                4,
-                10,
-                TimeUnit.MINUTES,
-                new ArrayBlockingQueue<>(2),
-                new customThreadFactory(),
-                new customRejectedExecutionHandler());
-        executor.allowCoreThreadTimeOut(true);
-        for (int i = 0; i < 7; i++) {
-            executor.submit(()->{
-                try{
-                    Thread.sleep(5000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println("Task processed by:" + Thread.currentThread().getName() );
-            });
-        }
-        }
+//        ThreadPoolExecutor executor = new ThreadPoolExecutor(
+//                2,
+//                4,
+//                10,
+//                TimeUnit.MINUTES,
+//                new ArrayBlockingQueue<>(2),
+//                new customThreadFactory(),
+//                new customRejectedExecutionHandler());
+//        executor.allowCoreThreadTimeOut(true);
+//        for (int i = 0; i < 7; i++) {
+//            executor.submit(() -> {
+//                try {
+//                    Thread.sleep(5000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//                System.out.println("Task processed by:" + Thread.currentThread().getName());
+//            });
+//        }
+        //  executor.shutdown();
+        custominterface obj = () -> System.out.println("hello");
+        obj.method();
+        Supplier<String> obj2 = ()-> "hello";
+        System.out.println(obj2.get());
+        Predicate<Integer> p = (i) -> {
+            if (i > 20) return true;
+            else return false;
+        };
+        System.out.println(p.test(2));
     }
+}
 class customThreadFactory implements ThreadFactory{
     @Override
     public Thread newThread(Runnable r) {
@@ -39,4 +51,8 @@ class customRejectedExecutionHandler implements RejectedExecutionHandler {
     public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
         System.out.println("Task " + r.toString() + " has been rejected");
     }
+}
+@FunctionalInterface
+interface custominterface{
+    void method();
 }
